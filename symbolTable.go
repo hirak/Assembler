db8e4c70544096f91803c644e15ea10255214734
Hiraku NAKANO <hiraku@tojiru.net>
2015-11-06 12:27:39 +0900

    implements SymbolTable module

---
+++ symbolTable.go
@@ -0,0 +1,46 @@
+package main
+
+type SymbolTable map[string]int
+
+func NewSymbolTable() SymbolTable {
+	table := SymbolTable{
+		"SP": 0x0000,
+		"LCL": 0x0001,
+		"ARG": 0x0002,
+		"THIS": 0x0003,
+		"THAT": 0x0004,
+		"R0": 0x0000,
+		"R1": 0x0001,
+		"R2": 0x0002,
+		"R3": 0x0003,
+		"R4": 0x0004,
+		"R5": 0x0005,
+		"R6": 0x0006,
+		"R7": 0x0007,
+		"R8": 0x0008,
+		"R9": 0x0009,
+		"R10": 0x000a,
+		"R11": 0x000b,
+		"R12": 0x000c,
+		"R13": 0x000d,
+		"R14": 0x000e,
+		"R15": 0x000f,
+		"SCREEN": 0x4000,
+		"KBD": 0x6000,
+	}
+
+	return table
+}
+
+func (s SymbolTable) AddEntry(symbol string, address int) {
+	s[symbol] = address
+}
+
+func (s SymbolTable) Contains(symbol string) bool {
+	_, ok := s[symbol]
+	return ok
+}
+
+func (s SymbolTable) GetAddress(symbol string) int {
+	return s[symbol]
+}
---
+++ symbolTable_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+//	"errors"
+//	"reflect"
+	"testing"
+)
+
+func TestSymbolTable(t *testing.T) {
+	s := NewSymbolTable()
+
+	s.AddEntry("LOOP", 16)
+	if !s.Contains("LOOP") {
+		t.Fatalf("s must contains LOOP")
+	}
+	actual := s.GetAddress("LOOP")
+	if actual != 16 {
+		t.Fatalf("Expecting LOOP address is 16, but %v", actual)
+	}
+}
